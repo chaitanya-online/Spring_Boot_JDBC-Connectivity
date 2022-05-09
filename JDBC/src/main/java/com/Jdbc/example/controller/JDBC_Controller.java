30aa361988011c313c56961dc12ec029dd5f7ca0
Chayn <chaithu.nampalli@gmail.com>
2022-05-09 11:00:24 +0530

    Addeed new Line 20 in JDBC_Controller class

--- JDBC/src/main/java/com/Jdbc/example/controller/JDBC_Controller.java
+++ JDBC/src/main/java/com/Jdbc/example/controller/JDBC_Controller.java
@@ -17,9 +17,15 @@ public class JDBC_Controller {
 
 		String Query = "insert into bcci (playerFirst_Name,LastName,jersey_No) values('Yuvaraj','Singh',12);";
 		String Query1 = "insert into bcci (playerFirst_Name,LastName,jersey_No) values('Sachin','Tendulkar',10);";
+		String Query2 = "insert into bcci (playerFirst_Name,LastName,jersey_No) values('Amthul','Saba',09);";
+
 
 		jdbc.execute(Query);
 		jdbc.execute(Query1);
+		jdbc.execute(Query2);
+
+		
+		
 
 		return "Data Inserted";
 
